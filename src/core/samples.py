93e113a18218e6480418e64d55abbd178b0cbae0
Daniel Londono <daniellondono17@gmail.com>
2023-11-12 15:17:21 -0500

    Argument added to fillna(0)

--- src/core/samples.py
+++ src/core/samples.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import worker
+import uploader as up
 
 # This file is purely for testing dont judge me plz
 
-# df = pd.read_excel('pivoted_try.xlsx')
+# df = pd.read_excel('visa_ret.xlsx')
 # periodos_fiscales = df['fp'].unique()
-
 
 # for f in periodos_fiscales:
 #     t = df[df['fp'] == f][['val','accn','label','fy','fp']]
@@ -14,9 +14,23 @@ import worker
 #     print('*-----**-----**-----**-----**-----**-----**-----**-----*')
 
 
-# print(df[df['accn']=='0001157523-09-007839']['label'].unique())
+
+
+
+# print(df[df['accn']=='0001193125-10-020834']['label'].unique())
 
 # res = df.pivot_table(index='fy', columns='label', values='val')
 
 # res = pd.read_excel('pivoted_results.xlsx')
 # print(res.dropna(axis=1).columns.to_list())
+# res.dropna(axis=1).to_excel('pivoted_dropedna_visa.xlsx')
+
+# transcript_nan_columns = res.dropna(axis=1, thresh=1)
+# transcript_nan_columns.to_excel('onlynans_visa.xlsx')
+
+
+
+i1 = worker.Worker('19617', 1, '10-K')
+df = i1.full_retrieval_()
+i2 = up.Uploader(df)
+i2.to_send_table_().to_excel('jpmorgan_pivoted.xlsx')
---
+++ src/core/uploader.py
@@ -0,0 +1,45 @@
+##############################################################################################################################   
+#
+#   Class for uploading datasets to the cloud
+#   Author: D. Santiago Londono
+#
+#   Project 4359 - BERT SEC
+#
+##############################################################################################################################
+
+import pandas as pd
+
+
+class Uploader:
+    
+    #
+    #   Constructor method of the class
+    #   @params
+    #       df: Dataframe - Dataframe returned from full_retrieval
+    #
+    def __init__(self, df):
+        self.df = df
+    
+    #
+    #   Pivots the input table and for each fiscal year it keeps the financial records
+    #
+    def pivot_table_(self)-> pd.DataFrame:
+        transcript = self.df.pivot_table(index='fy', columns='label', values='val')
+        return transcript
+
+    #
+    #   Replaces Nan Values with zeroes
+    #
+    def to_send_table_(self)-> pd.DataFrame:
+        '''
+        Argument:
+        In financial statements, certain items may not be reported for every fiscal year, 
+        leading to missing values. Filling these gaps with zeros is a pragmatic approach 
+        as it neither adds nor subtracts from the financial metrics, ensuring consistency 
+        and preserving the structure of the data.
+        '''
+        table = self.pivot_table_().fillna(0)
+        return table
+    
+
+
